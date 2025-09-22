c88b55e54125724e5a53d421d20530519b858f49
Lucas Nóbrega <lucasfernanndo07@gmail.com>
2025-09-22 17:52:29 -0300

    update do banco

--- infra/configs/connection.py
+++ infra/configs/connection.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from infra.configs.base import Base
 
 class DBConnectionHandler:
 
     def __init__(self) -> None:
-        self.__connection_string = 'sqlite:///kong_labs.db'
+        self.__connection_string = "sqlite:///kong_labs.db"
         self.__engine = self.__create_database_engine()
         self.session = None
 
@@ -19,6 +20,11 @@ class DBConnectionHandler:
         session_make = sessionmaker(bind=self.__engine)
         self.session = session_make()
         return self
+    
+    def create_db_tables(self):
+        engine = self.get_engine()
+        Base.metadata.create_all(engine)
+        print("Tabelas do banco de dados criadas com sucesso!")
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.session.close()
